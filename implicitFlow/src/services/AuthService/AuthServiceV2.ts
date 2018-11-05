1fb1605b9129f21687cc45f25d6ebda0f12294a3
Bob German <bgerman1@hotmail.com>
2018-11-05 01:18:08 -0500

    Collapsed ensureLogin

--- implicitFlow/src/services/AuthService/AuthServiceV2.ts
+++ implicitFlow/src/services/AuthService/AuthServiceV2.ts
@@ -8,37 +8,37 @@ export default class AuthService implements IAuthService {
         return new Promise<string>((resolve, reject) => {
 
             const userAgentApp = new Msal.UserAgentApplication(
-                constants.clientIdV2, 
-                null, null,
-                {
+                constants.clientIdV2, // Client ID
+                null,       // Authority
+                null,       // Token received callback
+                {           // Options
                     storeAuthStateInCookie: true,
-                    cacheLocation: "localStorage" 
+                    cacheLocation: "localStorage"
                 });
 
-            this.ensureLogin(userAgentApp);
+            // Ensure user is logged in
+            if (!userAgentApp.getUser() ||
+                userAgentApp.isCallback(window.location.hash)) {
+                userAgentApp.loginRedirect(constants.scopes);
+            }
 
+            // Try to get a token silently
             userAgentApp.acquireTokenSilent(constants.scopes)
-            .then ((accessToken) => {
-                resolve(accessToken);
-            })
-            .catch ((error) => {
-                console.log(error);
-                if (error.indexOf("consent_required") !== -1 || error.indexOf("interaction_required") !== -1 || error.indexOf("login_required") !== -1) {
-                    userAgentApp.acquireTokenRedirect(constants.scopes);
-                } else {
-                    reject('Error acquiring token: ' + error);
-                }
-            });
+                .then((accessToken) => {
+                    resolve(accessToken);
+                })
+                .catch((error) => {
+                    console.log(error);
+                    // If the error is due to a need for user
+                    // interaction, then redirect to allow it
+                    if (error.indexOf("consent_required") !== -1 ||
+                        error.indexOf("interaction_required") !== -1 ||
+                        error.indexOf("login_required") !== -1) {
+                        userAgentApp.acquireTokenRedirect(constants.scopes);
+                    } else {
+                        reject('Error acquiring token: ' + error);
+                    }
+                });
         });
     }
-
-    private ensureLogin(userAgentApp: Msal.UserAgentApplication): void { //: Promise<any> {
-
-        if (!userAgentApp.getUser() ||
-             userAgentApp.isCallback(window.location.hash)) {
-            userAgentApp.loginRedirect(constants.scopes);
-        }
-
-    }
-
 }
